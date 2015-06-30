25f6dda8970a1913d37c9519a4405d82598b69d0
huhu <huhu@huhu-PC>
2015-06-30 18:41:55 +0800

    文字滚动，数据颜色改为红色

--- js/home.js
+++ js/home.js
@@ -1,6 +1,4 @@
-var texta = ["初中语文 沪教版 六年级下_[2008年12月第3版] 更新至第2单元   407条","初中语文 人教版 八年级上[2009年3月第1版] 更新至第5单元   1450条","初中语文 人教版 七年级下_[2009年9月第1版] 更新至第2单元   573条"];
 var alen = texta.length;
-console.log(alen);
 function tabSlide(){
 	var tablist = $("#userSelect li");
 	var len = tablist.length;
@@ -20,16 +18,17 @@ var textroll = $(".rollBox a");
 var rolli = 1;
 textroll.text(texta[0]);
 
-//setInterval(function(){
-//	if(rolli == alen-1){
-//		textroll.text(texta[rolli]);
-//		rolli = 0;
-//	}
-//	else{
-//		textroll.text(texta[rolli]);
-//		rolli ++;
-//	}	
-//},4000)
+setInterval(function(){
+	textroll.animate({top: 0});
+	if(rolli == alen-1){
+		textroll.text(texta[rolli]);
+		rolli = 0;
+	}
+	else{		
+		textroll.text(texta[rolli]);
+		rolli ++;
+	}	
+},4000)
 
 $("#slides").slides({
 		preload: true,
